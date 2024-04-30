2e5a7c000d79d579b4e0417cfb213611d0638ec8
Danimal <dan.stoffels@gmail.com>
2024-04-29 21:12:33 -0500

    feat: implement direction parsing

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 from adventurer import Adventurer
 
 
+def parse_directions(sequence: str):
+    """Parses a directional sequence string (e.g. 12F4B for 12 steps forward, 4 back) into a list of tuples containing the number of steps as an int and the direction as a str."""
+
+    directions = []
+    steps = 0
+
+    for char in sequence:
+        try:
+            num = int(char)
+            if steps > 0:
+                steps *= 10
+            steps += num
+        except ValueError:
+            if char not in ["F", "B", "R", "L"]:
+                print(
+                    f"{char} is not a possible direction, please re-evaluate your sequence and try again."
+                )
+                exit()
+            directions.append((steps, char))
+            steps = 0
+
+    return directions
+
+
 def main():
     prompt = """Please enter a direction sequence to move your adventurer! Each instruction in the sequence starts with the number of steps followed by the direction.
     
